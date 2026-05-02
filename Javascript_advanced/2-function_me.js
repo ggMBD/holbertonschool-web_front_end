21749c4b752e5ff81ced03f5fefbd289ab1a1624
Maouia Ben Daoud <maouiabend@gmail.com>
2026-05-02 06:55:45 +0100

    Closure

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+function welcomeMessage (fullName){
+    return function(){
+        alert("Welcome "+fullName)
+    }
+}
+guillaume = welcomeMessage("Guillaume")
+alex = welcomeMessage("Alex")
+fred = welcomeMessage("Fred")
