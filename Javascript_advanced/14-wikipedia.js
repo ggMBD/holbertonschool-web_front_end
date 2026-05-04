f356f99e054479ef9828cc214265fddae3371b94
Maouia Ben Daoud <maouiabend@gmail.com>
2026-05-04 21:00:28 +0100

    Simple callback

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,29 @@
+function createElement(data){
+    const p = document.createElement("p");
+    p.textContent = data;
+    document.body.appendChild(p);
+
+}
+function queryWikipedia(callback) {
+  const xhr = new XMLHttpRequest();
+
+  const url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*";
+
+  xhr.open("GET", url);
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+
+      const pages = response.query.pages;
+      const pageId = Object.keys(pages)[0];
+      const extract = pages[pageId].extract;
+
+      callback(extract);
+    }
+  };
+
+  xhr.send();
+}
+
+queryWikipedia(createElement);
